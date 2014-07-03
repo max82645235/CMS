7f73835e179cd47b706458ecd37f088cb679856b
wangmingcha <wmcdyx@126.com>
2014-07-03 15:43:38 +0800

    fisrt commit

---
+++ demos/www/index.php
@@ -0,0 +1,11 @@
+<?php
+echo 11;exit;
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../../framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following line when in production mode
+// defined('YII_DEBUG') or define('YII_DEBUG',true);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
